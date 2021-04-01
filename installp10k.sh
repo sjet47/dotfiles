3cdd7fcf7a5ff0705f3960bb0a38661eedf0e828
ASjet <Aryan.ITFS@iCloud.com>
2021-04-01 15:11:14 +0800

    add setup script

---
+++ installp10k.sh
@@ -0,0 +1,4 @@
+#!/bin/zsh
+git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+source setup
