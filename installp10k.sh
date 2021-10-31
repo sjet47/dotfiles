65e64c66bd00d76e7d3b1db1bf7f6aacccc0cd4d
ASjet <Aryan.ITFS@iCloud.com>
2021-10-31 14:02:09 +0800

    update setup script

--- installp10k.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/zsh
-git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-git clone --depth=1 git://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
--- setup.sh
+++ setup.sh
@@ -1,13 +1,16 @@
 #!/bin/zsh
 
+# Install powerlevel10k
+git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+git clone --depth=1 git://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+# Define make link function
 function mklnk(){
 	if [[ -e $HOME/.$1 ]]; then
 		mv $HOME/.$1 $HOME/.$1.bak
 	fi
 	ln -s $HOME/dotfiles/$1 $HOME/.$1
 }
-
-source installp10k.sh
 
 mklnk gitconfig
 mklnk npmrc
@@ -16,15 +19,21 @@ mklnk vim
 mklnk vimrc
 mklnk zshrc
 mklnk gdbinit
+
+# User profile
 touch $HOME/.profile
+
+# User binary
 if [[ ! -e $HOME/.bin ]]; then
 	mkdir $HOME/.bin
 fi
 
+# User scripts
 if [[ ! -e $HOME/.script ]]; then
 	mkdir $HOME/.script
 fi
 
+# vimdir
 if [[ ! -e $HOME/.vimdir ]]; then
 	mkdir $HOME/.vimdir
 	mkdir $HOME/.vimdir/swp
@@ -32,5 +41,8 @@ if [[ ! -e $HOME/.vimdir ]]; then
 	mkdir $HOME/.vimdir/undo
 fi
 
+# Load zshrc
 source $HOME/.zshrc
+
+# Configure powerlevel10k
 p10k configure
