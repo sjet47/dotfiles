910ddfa9f7d50c392d96e5425153ee3063952db9
ASjet <Aryan.ITFS@iCloud.com>
2021-12-22 17:13:31 +0800

    feat(download): new item: pls

--- download.sh
+++ download.sh
@@ -11,16 +11,21 @@ then
 fi
 if [[ $disv == "Arch" ]]
 then
-	yay -Syyu --noconfirm zsh git lsd bat enca
+	yay -Syyu --noconfirm zsh git lsd bat enca wget
+	wget https://github.com/chenjiandongx/pls/releases/download/v0.1.4/pls_linux_amd64 -O $HOME/.bin/pls
 elif [[ $disv == "Ubuntu" ]]
 then
 	sudo apt update
 	sudo apt upgrade -y
-	sudo apt install zsh git lsd bat enca
+	sudo apt install -y zsh git lsd bat enca wget
+	wget https://github.com/chenjiandongx/pls/releases/download/v0.1.4/pls_linux_amd64 -O $HOME/.bin/pls
 elif [[ $disv == "macOS" ]]
 
 then
 	brew update
 	brew upgrade
 	brew install zsh git lsd bat enca
+	wget https://github.com/chenjiandongx/pls/releases/download/v0.1.4/pls_darwin_amd64 -O $HOME/.bin/pls
 fi
+chmod +x $HOME/.bin/pls
+$HOME/.bin/pls upgrade
