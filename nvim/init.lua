761b41e2da079a86169c48da379cdaf112ec9ffb
Aryan Sjet <sjet@asjet.dev>
2024-01-16 15:03:52 +0800

    feat(nvim/lazy): add colorscheme onedarkpro

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,5 +1,16 @@
 require("basic")
 
-if not vim.g.vscode then
-	require("v1").setup()
+-- Skip plugins if running with vscode
+if vim.g.vscode then
+	return
 end
+
+-- Set lazy.nvim path: $HOME/.config/nvim/lazy/lazy.nvim
+local lazypath = vim.fn.stdpath("config") .. "/lazy/lazy.nvim"
+vim.opt.rtp:prepend(lazypath)
+
+-- Load plugins
+require("lazy").setup("plugins", require("config"))
+
+-- Set colorscheme
+vim.cmd("colorscheme onedark")
