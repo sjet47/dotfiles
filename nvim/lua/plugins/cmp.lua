a5797cb121ca55743a1d110b0810aabf4eed69ba
Aryan Sjet <sjet@asjet.dev>
2024-01-17 15:44:55 +0800

    feat(nvim/lazy): add nvim-cmp plugin

---
+++ nvim/lua/plugins/cmp.lua
@@ -0,0 +1,122 @@
+return {
+    "saadparwaiz1/cmp_luasnip",
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
+    "hrsh7th/cmp-cmdline",
+    "hrsh7th/cmp-nvim-lsp-signature-help" ,
+    {
+        "hrsh7th/nvim-cmp",
+        dependencies = {
+            "saadparwaiz1/cmp_luasnip",
+            "hrsh7th/cmp-nvim-lsp",
+            "hrsh7th/cmp-buffer",
+            "hrsh7th/cmp-path",
+            "hrsh7th/cmp-cmdline",
+            "hrsh7th/cmp-nvim-lsp-signature-help" ,
+        },
+        config = function()
+            local cmp = require("cmp")
+            cmp.setup({
+                snippet = {
+                  -- REQUIRED - you must specify a snippet engine
+                  expand = function(args)
+                    -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+                    require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+                    -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+                    -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+                  end,
+                },
+                window = {
+                  completion = cmp.config.window.bordered(),
+                  documentation = cmp.config.window.bordered(),
+                },
+                mapping = cmp.mapping.preset.insert({
+                    ['<C-j>'] = cmp.mapping({
+                        c = function()
+                            if cmp.visible() then
+                                cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+                            else
+                                vim.api.nvim_feedkeys(t('<Down>'), 'n', true)
+                            end
+                        end,
+                        i = function(fallback)
+                            if cmp.visible() then
+                                cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+                            else
+                                fallback()
+                            end
+                        end
+                    }),
+                    ['<C-k>'] = cmp.mapping({
+                        c = function()
+                            if cmp.visible() then
+                                cmp.select_prev_item({ behavior = cmp.SelectBehavior.Select })
+                            else
+                                vim.api.nvim_feedkeys(t('<Up>'), 'n', true)
+                            end
+                        end,
+                        i = function(fallback)
+                            if cmp.visible() then
+                                cmp.select_prev_item({ behavior = cmp.SelectBehavior.Select })
+                            else
+                                fallback()
+                            end
+                        end
+                    }),
+                    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+                    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+                    ['<C-i>'] = cmp.mapping.complete(),
+                    ['<Esc>'] = cmp.mapping(
+                        function(fallback)
+                            -- abort and feed ESC
+                            cmp.abort()
+                            fallback()
+                        end
+                    , {"i"}),
+                    ['<Tab>'] = cmp.mapping(function(fallback)
+                        if cmp.visible() then
+                            cmp.confirm({ select = true })
+                        else
+                          fallback()
+                        end
+                      end, { "i", "s" }),
+                }),
+                sources = cmp.config.sources({
+                  { name = 'nvim_lsp' },
+                  { name = 'nvim_lsp_signature_help' },
+                  { name = 'luasnip' }, -- For luasnip users.
+                  -- { name = 'vsnip' }, -- For vsnip users.
+                  -- { name = 'ultisnips' }, -- For ultisnips users.
+                  -- { name = 'snippy' }, -- For snippy users.
+                }, {
+                  { name = 'buffer' },
+                  { name = 'path' },
+                  { name = 'cmdline' },
+                })
+            })
+
+            -- `/` cmdline setup.
+            cmp.setup.cmdline('/', {
+                mapping = cmp.mapping.preset.cmdline(),
+                sources = {
+                    { name = 'buffer' }
+                }
+            })
+            -- `:` cmdline setup.
+            cmp.setup.cmdline(':', {
+                mapping = cmp.mapping.preset.cmdline(),
+                sources = cmp.config.sources({
+                    { name = 'path' }
+                }, {
+                    {
+                        name = 'cmdline',
+                        option = {
+                            ignore_cmds = { 'Man', '!' }
+                        }
+                    }
+                })
+            })
+        end
+    },
+}
