fe32ddb4e9e28786b49f8feebc969c0a4c43e9db
Aryan Sjet <sjet@asjet.dev>
2024-01-17 13:59:45 +0800

    fix(nvim/lazy): bufferline error

--- nvim/lua/plugins/bufferline.lua
+++ nvim/lua/plugins/bufferline.lua
@@ -1,6 +1,7 @@
 return {
     'akinsho/bufferline.nvim',
     version = "*",
+    event = 'BufReadPre',
     dependencies = 'nvim-tree/nvim-web-devicons',
     config = function(LazyPlugin, opts)
         vim.opt.termguicolors = true
