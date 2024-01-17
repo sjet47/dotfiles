0fe1dd96eb340b228b628bd64450cf3614b22d91
Aryan Sjet <sjet@asjet.dev>
2024-01-17 23:20:56 +0800

    feat(nvim/lazy): add LuaSnip plugin

---
+++ nvim/lua/plugins/luasnip.lua
@@ -0,0 +1,9 @@
+return {
+    -- Snippet Engine for Neovim written in Lua.
+    ---@see https://github.com/L3MON4D3/LuaSnip
+	"L3MON4D3/LuaSnip",
+	-- follow latest release.
+	version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+	-- install jsregexp (optional!).
+	build = "make install_jsregexp"
+}
