717b14edbc39217d5d4a3cfbce8825b1a337159c
sjet47 <contact@asjet.dev>
2026-04-28 17:05:08 +0800

    feat(config): vicinae

---
+++ config/vicinae/scripts/code-django.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# @vicinae.schemaVersion 1
+# @vicinae.title Open heybox in VS Code
+# @vicinae.mode silent
+# @vicinae.exec ["/bin/bash"]
+# @vicinae.icon ../icons/VSCode-Dark.svg
+
+code ~/heybox/repo/heybox
---
+++ config/vicinae/scripts/code-script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# @vicinae.schemaVersion 1
+# @vicinae.title Open heybox_script in VS Code
+# @vicinae.mode silent
+# @vicinae.exec ["/bin/bash"]
+# @vicinae.icon ../icons/VSCode-Dark.svg
+
+code ~/heybox/repo/heybox_script
