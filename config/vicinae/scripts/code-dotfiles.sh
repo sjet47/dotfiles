22375f9eea80e357781c381f824b7e9ac1fdffa9
sjet47 <contact@asjet.dev>
2026-06-20 16:45:19 +0800

    feat(config): add vicinae script alias

---
+++ config/vicinae/scripts/code-dotfiles.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# @vicinae.schemaVersion 1
+# @vicinae.title Open dotfiles in VS Code
+# @vicinae.mode silent
+# @vicinae.exec ["/bin/bash"]
+# @vicinae.icon ../icons/VSCode-Dark.svg
+
+code ~/dotfiles
