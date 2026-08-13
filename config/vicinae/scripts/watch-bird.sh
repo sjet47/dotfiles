842efae03205e06358562acbf353e0ef38a44762
sjet47 <contact@asjet.dev>
2026-08-13 20:39:39 +0800

    feat(vicinae): add watch bird script

---
+++ config/vicinae/scripts/watch-bird.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# @vicinae.schemaVersion 1
+# @vicinae.title Watch Cute Bird
+# @vicinae.mode silent
+# @vicinae.exec ["/bin/bash"]
+# @vicinae.icon ../icons/mpv.png
+
+~/repo/mi-home-camera/scripts/watch.sh camera4 mpv
